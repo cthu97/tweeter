b432a93e067692a3c3a4c306e27d2bcddbcd6c07
Catherine Hu <catherinehu_9898@hotmail.com>
2021-07-15 00:21:04 +0000

    hardcoded render tweet + time

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,85 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+createTweetElement = function (tweet) {
+  const text = $("<div>").text(tweet.content.text).html();
+  const name = $("<div>").text(tweet.user.name).html();
+  const username = $("<div>").text(tweet.user.handle).html();
+  const avatar = $("<div>").text(tweet.user.avatars).html();
+  const time = $("<div>").text(tweet.created_at).html();
+
+
+  let $tweet = $(`<section class="tweets">
+  <article>
+    <div>
+      <img class="avatar" src="${avatar}">
+      <h3 class="name">${name}</h3>
+      <p class="username">${username}</p>
+    </div>
+    <p class="tweet-body">
+        ${text}
+      </p>
+      <footer>
+        <p class="footer-text">${timeago.format(time)}</p>
+        <div>
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </footer>
+    </article>
+  </section>`
+  )
+  return $tweet
+}
+
+const renderTweets = function (tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  $('.tweet-container').empty()
+  for (let tweet of tweets) {
+    const $tweet = createTweetElement(tweet)
+    $('.tweet-container').prepend($tweet)
+  }
+}
+
+
+$(document).ready(() => {
+  const $tweet = renderTweets(tweetData)
+  //$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+})
+
+$("new-tweet form").on("submit", (event) => {
+  event.preventDefault();
+
+})
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,13 +4,21 @@ $(document).ready(function () {
     const count = 140;
     const $counter = $('.counter')
     let inputLength = $(this).val().length;
-    $counter.text(count - inputLength)
+    const $input = $('#tweet-text')
+    
+    //counts the characters and decreases char limits accordingly
+    $counter.text(count - inputLength);
 
+    
+    
     //changes colour to red if input goes over char limit
     if (inputLength > 140) {
       $counter.css('color', 'red');
     } else {
       $counter.css('color', 'black')
     }
+    
   })
+/*   //clears and refocuses text box
+  $input.val('').focus(); */
 });
